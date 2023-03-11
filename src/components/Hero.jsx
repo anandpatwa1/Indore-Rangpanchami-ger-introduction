a207b4e8589c67409edc8eb4f84de39b40fa5a8e
anandpatwa1 <anandpatwa.js@gmail.com>
2023-03-12 02:00:10 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className="hero min-h-screen bg-center" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1616787671803-e660b92c0d25?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=3000&ixid=MnwxfDB8MXxyYW5kb218MHx8aG9saXx8fHx8fDE2NzgzNjM5OTA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=3000")` }}>
+    <div className="hero min-h-screen bg-center bg-fixed" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1616787671803-e660b92c0d25?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=3000&ixid=MnwxfDB8MXxyYW5kb218MHx8aG9saXx8fHx8fDE2NzgzNjM5OTA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=3000")` }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
