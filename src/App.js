50caa0761a8a40adeeb1bba0adbcc8b41e5230f3
anand patwa <119409772+anandpatwa1@users.noreply.github.com>
2023-08-28 00:56:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
 import News from "./components/News";
 
 function App() {
   return (
     <>
     <div data-theme="corporate">
-       <Navbar/>
        <Hero/>
        <News/>
        <Footer/>
